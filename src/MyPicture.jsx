c1e1c9c5de1b1d2931d90c47d8cc32b600927336
Naman Rusia <naman.rusia14@gmail.com>
2024-12-01 14:23:21 -0500

    small edit

--- src/MyPicture.jsx
+++ src/MyPicture.jsx
@@ -5,9 +5,18 @@ import HoverImage from "./assets/myPicture2.jpeg"; // Hover image
 
 const MyPicture = () => {
   return (
-    <div id="my-picture" className="image-container">
-      <img src={MyImage} alt="A picture of me" className="default-image" />
-      <img src={HoverImage} alt="Hover picture of me" className="hover-image" />
+    <div>
+      <div id="my-picture" className="image-container">
+        <img src={MyImage} alt="A picture of me" className="default-image" />
+        <img
+          src={HoverImage}
+          alt="Hover picture of me"
+          className="hover-image"
+        />
+      </div>
+      <div>
+        <p>WORK IN PROGRESS</p>
+      </div>
     </div>
   );
 };
