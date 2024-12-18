5234cae71e1106908c8f768e4393f1e8c600eede
Naman Rusia <namanrusia@MacBook-Pro-4.local>
2024-12-18 17:02:06 +0800

    changes not showing up on netlify, troubleshooting

--- src/routes/Resume.jsx
+++ src/routes/Resume.jsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import styled from "styled-components";
-import { FaRocket, FaBrain, FaLightbulb, FaDove } from "react-icons/fa";
 import TopNav from "../TopNav";
-import { div } from "framer-motion/client";
 import SkillsCard from "../SkillsCard";
 
 const Resume = () => {
@@ -33,25 +29,22 @@ const Resume = () => {
 
   const [experience, setExperience] = useState([
     {
-      title: "Senior Frontend Developer",
-      company: "Tech Innovators Inc.",
-      date: "2020 - Present",
-      description:
-        "Led a team of developers in creating responsive and scalable web applications using React and modern JavaScript.",
+      title: "",
+      company: "",
+      date: "",
+      description: "",
     },
     {
-      title: "Full Stack Developer",
-      company: "Digital Solutions Ltd.",
-      date: "2018 - 2020",
-      description:
-        "Developed and maintained full-stack applications using MERN stack, improving overall performance by 40%.",
+      title: "",
+      company: "",
+      date: "",
+      description: "",
     },
     {
-      title: "Junior Developer",
-      company: "StartUp Ventures",
+      title: "",
+      company: "",
       date: "2016 - 2018",
-      description:
-        "Assisted in the development of mobile applications using Swift and contributed to backend services using Python.",
+      description: "",
     },
   ]);
 
@@ -86,7 +79,7 @@ const Resume = () => {
       <div>
         {/* From Uiverse.io by Javierrocadev */}
         <button
-          class="group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-rose-300 hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 underline underline-offset-2 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur hover:underline hover:underline-offset-4  origin-left hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 h-16 w-64 border text-left p-3 text-gray-50 text-base font-bold rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-rose-300 after:right-8 after:top-3 after:rounded-full after:blur-lg"
+          className=""
           onClick={() => {
             window.open("/public/resume_12:18:2024.pdf", "_blank");
           }}
